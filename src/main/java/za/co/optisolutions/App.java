5920ccde77a971fded764298da2da1f527b71b1e
215138848 <215138848@mycput.ac.za>
2021-07-27 10:07:34 +0200

    first commit

---
+++ src/main/java/za/co/optisolutions/App.java
@@ -0,0 +1,13 @@
+package za.co.optisolutions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
