62e0b249c182939388551d60135354c9d791eea5
215138848 <215138848@mycput.ac.za>
2021-07-28 11:40:49 +0200

    4 tests done

--- src/main/java/za/co/optisolutions/MyGameOfLife.java
+++ src/main/java/za/co/optisolutions/MyGameOfLife.java
@@ -11,9 +11,45 @@ public class MyGameOfLife {
 
     public static void main( String[] args )    {
         System.out.println( "Welcome to Conway's Game of Life." );
+        GameIntro();
+    }
+
+    public static void GameIntro(){
+
         Scanner scan = new Scanner(System.in);
 
+        System.out.println( "What type of game would you like to play: " );
+
+        System.out.println( "1: Random Generated board" );
+        System.out.println( "2: L Shaped board" );
+        System.out.println( "3: M Shaped board" );
+        System.out.println( "4: Manual input of board" );
+        System.out.println( "5: Exit" );
+
+        System.out.println( "Please input the number for the game you would like to play. " );
+        int gameType = scan.nextInt();
+
+            switch (gameType) {
+                case 1: Game1Random();
+                    break;
+                case 2: GameLBoard();
+                    break;
+                case 3: GameMBoard();
+                    break;
+                case 4: Game4Manual();
+                    break;
+                default:    System.out.println( "       " );
+                            System.out.println( "          GAME OVER       " );
+                            System.out.println( "       " );
+                            System.out.println( "    Thank You for Playing    " );
+            }
+    }
+
+    public static void Game1Random(){
+
+        Scanner scan = new Scanner(System.in);
         boolean continue1 = true;
+
         while (continue1){
             System.out.println( "Please enter the size of the board?" );
             System.out.print( "Number of Rows: " );
@@ -30,16 +66,11 @@ public class MyGameOfLife {
             for(int i=0; i<=generations; i++){
                 System.out.println("Generation " + i + ":");
                 //printing board
-                for (int rows =0; rows<rowSize; rows++){
-                    for (int columns=0; columns<columnSize; columns++){
-                        System.out.print(board[rows][columns] + "");
-                    }
-                    System.out.println("");
-                }
-                board = life(board, rowSize, columnSize);
+                System.out.println(PrintBoard(board));
+                board = life(board);
             }
 
-            System.out.println( "Play again? y/n" );
+            System.out.println( "Play Random-Board Again? y/n" );
             String ans = scan.next();
             if(ans.equalsIgnoreCase("y")){
                 continue1=true;
@@ -48,14 +79,100 @@ public class MyGameOfLife {
                 continue1=false;
             }
         }
-        System.out.println( "       " );
-        System.out.println( "          GAME OVER       " );
-        System.out.println( "       " );
-        System.out.println( "    Thank You for Playing    " );
+        GameIntro();
     }
 
-    public static String[][] life(String [][] board, int rowSize, int columnSize){
+    public static void GameLBoard(){
 
+        String [][] LBoard = newLBoard();
+
+        for(int i=0; i<=5; i++){
+            System.out.println("Generation " + i + ":");
+            //printing board
+            System.out.println(PrintBoard(LBoard));
+            LBoard = life(LBoard);
+        }
+        GameIntro();
+    }
+
+    public static String[][] newLBoard(){
+        String [][] newBoard = new String [][] {{"_","_","_","_","_"}, {"_","_","#","_","_"}, {"_","_","_","#","_"}, {"_","#","#","#","_"}, {"_","_","_","_","_"} };
+        return newBoard;
+    }
+
+    public static void GameMBoard(){
+
+        String [][] MBoard = new String [][] {{"#","_","_","_","#"}, {"#","#","_","#","#"}, {"#","_","#","_","#"}, {"#","_","_","_","#"}, {"#","_","_","_","#"} };
+        int rowSize = MBoard.length;
+        int columnSize = MBoard[0].length;
+
+        for(int i=0; i<=4; i++){
+            System.out.println("Generation " + i + ":");
+            //printing board
+            System.out.println(PrintBoard(MBoard));
+            MBoard = life(MBoard);
+        }
+        GameIntro();
+    }
+
+    public static void Game4Manual(){
+
+        Scanner scan = new Scanner(System.in);
+        boolean continue1 = true;
+
+        while (continue1){
+            System.out.println( "Welcome to Manual-board Game of Life " );
+            System.out.println( "Please enter the details of the board? the board is 5x5 in size." );
+
+            String [][] board = new String[5][5];
+
+            for (int r=0; r<5; r++){
+                for (int c=0; c<5; c++){
+                    //enter the details of each cell
+                    System.out.println( "Please enter 1 for Alive and 0 for dead?" + "\nCurrent cell is x:" + r + ", y: "+ c );
+                    String temp = scan.next();
+
+                    if (temp.isEmpty()){
+                        board[r][c] = "_";
+                    }
+                    else {
+                        if (temp.equals("1")){
+                            board[r][c] = "#";
+                        }
+                        if (temp.equals("0")){
+                            board[r][c] = "_";
+                        }
+                        if (!temp.equals("1") && !temp.equals("0")){
+                            board[r][c] = "#";
+                        }
+                    }
+                }//end of c
+            }//end of r
+
+            System.out.println( "Manual Board is done: Generation 0" );
+            System.out.println(PrintBoard(board));
+            System.out.println("Generation 1:");
+            board = life(board);
+            System.out.println(PrintBoard(board));
+
+
+
+            System.out.println( "Play Manual-Board Again? y/n" );
+            String ans = scan.next();
+            if(ans.equalsIgnoreCase("y")){
+                continue1=true;
+            }
+            else {
+                continue1=false;
+            }
+        }
+        GameIntro();
+    }
+
+    public static String[][] life(String [][] board){
+
+        int rowSize = board.length;
+        int columnSize = board[0].length;
         String [][] newBoard = new String[rowSize][columnSize];
 
         for (int row =0; row< rowSize; row++){
@@ -64,7 +181,7 @@ public class MyGameOfLife {
 
                 if(board[row][i].equals("#")){
                     //if space is populated
-                    int neighbourCount = neighbourCounter(board, row, i, rowSize, columnSize);
+                    int neighbourCount = neighbourCounter(board, row, i);
                     if(neighbourCount<=1){
                         newBoard[row][i] = "_"; //is dead
                     }
@@ -77,7 +194,7 @@ public class MyGameOfLife {
                 }
                 if(board[row][i].equals("_")){
                     //if space is unpopulated
-                    int neighbourCount = neighbourCounter(board, row, i, rowSize, columnSize);
+                    int neighbourCount = neighbourCounter(board, row, i);
                     if(neighbourCount==3 ){
                         newBoard[row][i] = "#"; //is alive
                     }
@@ -93,8 +210,24 @@ public class MyGameOfLife {
         return newBoard;
     }
 
+    public static String PrintBoard(String [][] board){
+        int rowSize = board.length;
+        int columnSize = board[0].length;
+        String map = "";
+
+        //printing board
+        for (int rows =0; rows<rowSize; rows++){
+            for (int columns=0; columns<columnSize; columns++){
+                map += (board[rows][columns] + " ");
+            }
+            map += ("\n");
+        }
+
+        return map;
+    }
+
     public static String [][] GenNewBoard(int rowSize, int columnSize){
-        //String [][] newBoard = new String [][] {{"_","#","#","_","_"}, {"_","#","_","#","_"}, {"_","#","_","#","_"}, {"_","#","_","#","_"}, {"_","#","#","_","#"} };
+        //String [][] setBoard = new String [][] {{"_","#","#","_","_"}, {"_","#","_","#","_"}, {"_","#","_","#","_"}, {"_","#","_","#","_"}, {"_","#","#","_","#"} };
 
         String [][] newBoard = new String [rowSize][columnSize] ;
 
@@ -111,7 +244,10 @@ public class MyGameOfLife {
         return newBoard;
     }
 
-    public static int neighbourCounter(String [][] board, int x, int y, int rowSize, int columnSize){
+    public static int neighbourCounter(String [][] board, int x, int y){
+        int rowSize = board.length;
+        int columnSize = board[0].length;
+
         int neighbourCount = 0;
         if(y!=0){
             if(x!=0){
--- src/test/java/za/co/optisolutions/MyGameOfLifeTest.java
+++ src/test/java/za/co/optisolutions/MyGameOfLifeTest.java
@@ -1,20 +1,61 @@
 package za.co.optisolutions;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import za.co.optisolutions.MyGameOfLife;
 
 /**
- * Unit test for simple App.
+ * Grant Hendricks
+ * Unit test for Game of Life App.
  */
-public class MyGameOfLifeTest
-{
-    /**
-     * Rigorous Test :-)
-     */
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class MyGameOfLifeTest{
+
+    String [][] board = new String[5][5];
+    MyGameOfLife gameTester = new MyGameOfLife();
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void a_FillBoardGameTest() {
+        System.out.println( "Test 1: Filling board" );
+        board = gameTester.newLBoard();
+        assertNotNull(board[1][2]);
+        assertNotNull(board[2][3]);
+        assertNotNull(board[3][1]);
+        assertNotNull(board[3][2]);
+        assertNotNull(board[3][3]);
+        System.out.println("The board has been filled\n" + gameTester.PrintBoard(board));
     }
+
+    @Test
+    public void b_Neighbors(){
+        board = gameTester.newLBoard();
+        System.out.println( "Test 2: Counting of neighbors" );
+        assertEquals(gameTester.neighbourCounter(board,3,2), 3);
+        System.out.println("The board has been filled\n" + gameTester.PrintBoard(board));
+        System.out.println( "position [3][2] has " + 5 + " neighbours" );
+    }
+
+    @Test
+    public void c_PrintBoardGameTest() {
+        System.out.println( "Test 3: Counting of neighbors" );
+        board = gameTester.newLBoard();
+        assertNotNull(gameTester.PrintBoard(board));
+        System.out.println("this is the board printed\n" + gameTester.PrintBoard(board));
+    }
+
+    @Test
+    public void d_LifeGameTest() {
+        System.out.println( "Test 4: Executing Life" );
+        board = gameTester.newLBoard();
+        String [][] originalBoard = board;
+        board = gameTester.life(board);
+        assertEquals(originalBoard[3][3], board[2][3] );
+        System.out.println("Game of life is successfull\n" + gameTester.PrintBoard(originalBoard) + "\n\nGeneration 2 Board: \n" + gameTester.PrintBoard(board) );
+    }
+
 }
